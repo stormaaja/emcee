7ba19c2ec735e3690ea6703d4e7e27781dd14621
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-20 22:01:38 +0200

    Use jison parser

--- parse.js
+++ parse.js
@@ -3,13 +3,15 @@ if (!process.argv[2]) {
   return 1;
 }
 
-const parser = require('./emcee.js');
+const jison = require("jison");
 const fs = require('fs');
 const path = require('path');
 const file = path.normalize(process.argv[2]);
 const source = fs.readFileSync(file, "utf8");
+const bnf = fs.readFileSync("emcee.jison", "utf8");
 
 try {
+  const parser = new jison.Parser(bnf);
   const result = parser.parse(source);
   console.log("OK");
 } catch(err) {
