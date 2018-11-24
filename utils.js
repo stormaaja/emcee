ae88e79537f9b099e1f6272ee9f209445db40b7f
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-24 16:08:51 +0200

    Add utils for creating nodes

---
+++ utils.js
@@ -0,0 +1,29 @@
+function createNode(nodeType, children, id, meta, expr) {
+  return { nodeType, children, id, meta, expr};
+}
+
+function appendNode(children, x) {
+  return children.concat(x);
+}
+
+function createReturn(expr) {
+  return createNode(
+    "return", [], undefined, {}, expr);
+}
+
+function createFunction(children, id, returnType, argList) {
+  return createNode("function", children, id, {returnType, argList});
+}
+
+function createComparision(comparisionType, left, right) {
+  return createNode(
+    "comparision", [], undefined, {left, right, comparisionType});
+}
+
+function createFnCall(id, params) {
+  return createNode("fn_call", [], id, {params});
+}
+
+module.exports = {
+  createNode, appendNode, createFunction, createComparision, createFnCall
+}
