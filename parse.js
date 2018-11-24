62c7ea04998899bc4c1652a0381831f967c323ba
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-24 16:23:26 +0200

    Make parser a bit more flexible

--- parse.js
+++ parse.js
@@ -14,33 +14,42 @@ function loadFile(filePath) {
 }
 
 const bnf = fs.readFileSync("emcee.jison", "utf8");
-const parser = new jison.Parser(bnf);
+
+function parseSource(parser, source) {
+  try {
+    return {success: true, result: parser.parse(source)};
+  } catch(err) {
+    return {success: false, result: err};
+  }
+}
 
 function parseFile(file) {
+  const parser = new jison.Parser(bnf);
   const source = loadFile(file);
-  let success = true;
-  let result;
 
-  try {
-    const parseResult = parser.parse(source);
-    result = parseResult;
-  } catch(err) {
-    success = false;
-    result = err;
-  }
-
-  return {"file": file, "success": success, "result": result};
+  return Object.assign(
+    {file, parser}, parseSource(parser, source)
+  );
 }
 
 function printGreen(text) {
   console.log("\x1b[32m%s\x1b[32m", text);
 }
 
-const files = process.argv.slice(2);
+const args = process.argv.slice(2);
+const files = args.filter(f => !f.startsWith("--"));
+const options = args.filter(f => f.startsWith("--"))
+
+const printTree = options.includes("--print-tree");
+
 const results = files.map(parseFile);
+
 results.forEach(r => {
   if (r.success) {
     console.log("%s: \x1b[32mOK\x1b[0m", r.file);
+    if (printTree) {
+      console.log(JSON.stringify(r.result, null, 2));
+    }
   } else {
     console.log("%s: \x1b[31mFail\x1b[0m", r.file);
     console.log(r.result.message);
