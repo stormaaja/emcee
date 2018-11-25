c0f8c200fcf6bdf43297c1df987d78357db48e46
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-25 08:46:24 +0200

    Fix formatting

--- utils.js
+++ utils.js
@@ -1,29 +1,32 @@
+"use strict"
+
 function createNode(nodeType, children, id, meta, expr) {
-  return { nodeType, children, id, meta, expr};
+  return { nodeType, children, id, meta, expr}
 }
 
 function appendNode(children, x) {
-  return [x].concat(children);
+  return [x].concat(children)
 }
 
 function createReturn(expr) {
   return createNode(
-    "return", [], undefined, {}, expr);
+    "return", [], undefined, {}, expr)
 }
 
 function createFunction(children, id, returnType, argList) {
-  return createNode("function", children, id, {returnType, argList});
+  return createNode("function", children, id, {returnType, argList})
 }
 
 function createComparision(comparisionType, left, right) {
   return createNode(
-    "comparision", [], undefined, {left, right, comparisionType});
+    "comparision", [], undefined, {left, right, comparisionType})
 }
 
 function createFnCall(id, params) {
-  return createNode("fn_call", [], id, {params});
+  return createNode("fn_call", [], id, {params})
 }
 
-module.exports = {
-  createNode, appendNode, createFunction, createComparision, createFnCall
+export {
+  createNode, appendNode, createFunction, createComparision, createFnCall,
+  createReturn
 }
