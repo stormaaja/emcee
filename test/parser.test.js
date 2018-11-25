ea656d263ad34bf310b05fcd3d1d59b2db6dd90e
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-25 16:40:41 +0200

    Add if else test

--- test/parser.test.js
+++ test/parser.test.js
@@ -9,12 +9,27 @@ test("generates AST for if", () => {
   const source = generateMain("if (2 > 1) {return 0;} return 1;")
   expect(parser.parse(source).result).toEqual(
     createNode("root", [
-        createNode("function", [
-          createNode("if", [
-            createNode("return", [], null, {}, "0")
-          ], null, {}, createNode("compare_gt", ["2", "1"])),
+      createNode("function", [
+        createNode("if", [
+          createNode("return", [], null, {}, "0")
+        ], null, {}, createNode("compare_gt", ["2", "1"])),
+        createNode("return", [], null, {}, "1")
+      ], "main", {argList: [], returnType: "int"})
+    ])
+  )
+})
+
+test("generates AST of if-else", () => {
+  const source = generateMain("if (2 > 1) {return 0;} else {return 1;}")
+  expect(parser.parse(source).result).toEqual(
+    createNode("root", [
+      createNode("function", [
+        createNode("if", [
+          createNode("return", [], null, {}, "0"),
           createNode("return", [], null, {}, "1")
+          ], null, {}, createNode("compare_gt", ["2", "1"])),
         ], "main", {argList: [], returnType: "int"})
     ])
   )
+
 })
