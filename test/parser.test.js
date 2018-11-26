15844ba12bdd1df594d39a29789c2de023e5d564
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-26 19:16:03 +0200

    Move tests

--- test/parser.test.js
+++ test/parser.test.js
@@ -5,20 +5,6 @@ function generateMain(block) {
   return `int main() { ${block} }`
 }
 
-test("generates AST for if", () => {
-  const source = generateMain("if (2 > 1) {return 0;} return 1;")
-  expect(parser.parse(source).result).toEqual(
-    createNode("root", [
-      createNode("function", [
-        createNode("if", [
-          createNode("return", [], null, {}, "0")
-        ], null, {}, createNode("compare_gt", ["2", "1"])),
-        createNode("return", [], null, {}, "1")
-      ], "main", {argList: [], returnType: "int"})
-    ])
-  )
-})
-
 test("generates AST of if-else greater than", () => {
   const source = generateMain("if (2 > 1) {return 0;} else {return 1;}")
   expect(parser.parse(source).result).toEqual(
@@ -28,20 +14,6 @@ test("generates AST of if-else greater than", () => {
           createNode("return", [], null, {}, "0"),
           createNode("return", [], null, {}, "1")
           ], null, {}, createNode("compare_gt", ["2", "1"])),
-        ], "main", {argList: [], returnType: "int"})
-    ])
-  )
-})
-
-test("generates AST of compare less than", () => {
-  const source = generateMain("if (2 < 1) {return 1;} else {return 0;}")
-  expect(parser.parse(source).result).toEqual(
-    createNode("root", [
-      createNode("function", [
-        createNode("if", [
-          createNode("return", [], null, {}, "1"),
-          createNode("return", [], null, {}, "0")
-          ], null, {}, createNode("compare_lt", ["2", "1"])),
         ], "main", {argList: [], returnType: "int"})
     ])
   )
---
+++ test/parser_compare.test.js
@@ -0,0 +1,34 @@
+const parser = require('../parser.js');
+const {createNode} = require('../utils.js')
+
+function generateMain(block) {
+  return `int main() { ${block} }`
+}
+
+test("generates AST of compare greater than", () => {
+  const source = generateMain("if (2 > 1) {return 0;} return 1;")
+  expect(parser.parse(source).result).toEqual(
+    createNode("root", [
+      createNode("function", [
+        createNode("if", [
+          createNode("return", [], null, {}, "0")
+        ], null, {}, createNode("compare_gt", ["2", "1"])),
+        createNode("return", [], null, {}, "1")
+      ], "main", {argList: [], returnType: "int"})
+    ])
+  )
+})
+
+test("generates AST of compare less than", () => {
+  const source = generateMain("if (2 < 1) {return 1;} else {return 0;}")
+  expect(parser.parse(source).result).toEqual(
+    createNode("root", [
+      createNode("function", [
+        createNode("if", [
+          createNode("return", [], null, {}, "1"),
+          createNode("return", [], null, {}, "0")
+          ], null, {}, createNode("compare_lt", ["2", "1"])),
+        ], "main", {argList: [], returnType: "int"})
+    ])
+  )
+})
