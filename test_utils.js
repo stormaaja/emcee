172101f7b3395314a59a070bd0592e18d53b76f8
Matti Ahinko <matti.ahinko@gmail.com>
2018-11-27 20:04:09 +0200

    Add function for generating main node

--- test_utils.js
+++ test_utils.js
@@ -1,5 +1,14 @@
+const {createNode} = require("./utils.js")
+
 function generateMain(block) {
   return `int main() { ${block} }`
 }
 
-module.exports = {generateMain}
+function generateProgramNode(children) {
+  return createNode("root", [
+    createNode(
+      "function", children, "main", {argList: [], returnType: "int"})
+  ])
+}
+
+module.exports = {generateMain, generateProgramNode}
