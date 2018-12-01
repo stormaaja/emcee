69da98cfadb54d0c8a06a01ffa21c21a302ee272
Matti Ahinko <matti.ahinko@gmail.com>
2018-12-01 09:09:27 +0200

    Add info with line number and use it with errors

--- ast.js
+++ ast.js
@@ -3,72 +3,75 @@ const { Map, List } = require("immutable")
 
 // TODO change typechecks to return results instead of success boolean
 
+let currentLine = 0
+
 function generateNode(data) {
   const {nodeType, id, meta} = data
   const children = List(data.children)
+  const info = Map({line: currentLine++})
   switch (nodeType) {
   case "root": {
-    return new RootNode(List(children))
+    return new RootNode(children)
   }
   case "function": {
-    return new FunctionNode(id, List(children), meta)
+    return new FunctionNode(info, id, List(children), meta)
   }
   case "function_call": {
-    return new FunctionCallNode(id, List(children))
+    return new FunctionCallNode(info, id, List(children))
   }
   case "return": {
-    return new ReturnNode(children.first())
+    return new ReturnNode(info, children.first())
   }
   case "string_value": {
-    return new ValueNode("string", children.first())
+    return new ValueNode(info, "string", children.first())
   }
   case "integer_value": {
-    return new ValueNode("integer", children.first())
+    return new ValueNode(info, "integer", children.first())
   }
   case "double_value": {
-    return new ValueNode("double", children.first())
+    return new ValueNode(info, "double", children.first())
   }
   case "boolean_value": {
-    return new ValueNode("boolean", children.first())
+    return new ValueNode(info, "boolean", children.first())
   }
   case "array_values": {
-    return new ValueNode("array", children)
+    return new ValueNode(info, "array", children)
   }
   case "array_access": {
-    return new ArrayAccessNode(id, children.first())
+    return new ArrayAccessNode(info, id, children.first())
   }
   case "argument": {
-    return new ArgumentNode(id, meta.valueType)
+    return new ArgumentNode(info, id, meta.valueType)
   }
   case "assignment": {
-    return new AssignmentNode(id, children[1], meta.valueType)
+    return new AssignmentNode(info, id, children[1], meta.valueType)
   }
   case "compare_gt": {
-    return new CompareNode("gt", children.first(), children.get(1))
+    return new CompareNode(info, "gt", children.first(), children.get(1))
   }
   case "compare_lt": {
-    return new CompareNode("lt", children.first(), children.get(1))
+    return new CompareNode(info, "lt", children.first(), children.get(1))
   }
   case "compare_eq": {
-    return new CompareNode("eq", children.first(), children.get(1))
+    return new CompareNode(info, "eq", children.first(), children.get(1))
   }
   case "if": {
-    return new IfNode(children.first(), children.get(1))
+    return new IfNode(info, children.first(), children.get(1))
   }
   case "while": {
-    return new WhileNode(children.first(), children.get(1))
+    return new WhileNode(info, children.first(), children.get(1))
   }
   case "add_expr": {
-    return new ArithmeticsNode("add", children.first(), children.get(1))
+    return new ArithmeticsNode(info, "add", children.first(), children.get(1))
   }
   case "sub_expr": {
-    return new ArithmeticsNode("subtract", children.first(), children.get(1))
+    return new ArithmeticsNode(info, "subtract", children.first(), children.get(1))
   }
   case "mul_expr": {
-    return new ArithmeticsNode("multiply", children.first(), children.get(1))
+    return new ArithmeticsNode(info, "multiply", children.first(), children.get(1))
   }
   case "div_expr": {
-    return new ArithmeticsNode("div", children.first(), children.get(1))
+    return new ArithmeticsNode(info, "div", children.first(), children.get(1))
   }
 
   default: {
@@ -96,7 +99,8 @@ class RootNode {
 }
 
 class FunctionNode {
-  constructor(id, children, meta) {
+  constructor(info, id, children, meta) {
+    this.info = info
     this.id = id
     this.children = children
     this.returnType = meta.returnType
@@ -111,20 +115,23 @@ class FunctionNode {
 }
 
 class ReturnNode{
-  constructor(expression) {
+  constructor(info, expression) {
+    this.info = info
     this.expression = expression
   }
 }
 
 class FunctionCallNode {
-  constructor(id, params) {
+  constructor(info, id, params) {
+    this.info = info
     this.id = id
     this.params = params
   }
 }
 
 class ArrayNode {
-  constructor(id, elements, type) {
+  constructor(info, id, elements, type) {
+    this.info = info
     this.id = id
     this.elements = elements
     this.type = type
@@ -132,21 +139,24 @@ class ArrayNode {
 }
 
 class ArrayAccessNode {
-  constructor(id, expression) {
+  constructor(info, id, expression) {
+    this.info = info
     this.id = id
     this.expression = expression
   }
 }
 
 class WhileNode {
-  constructor(expression, body) {
+  constructor(info, expression, body) {
+    this.info = info
     this.expression = expression
     this.body = body
   }
 }
 
 class IfNode {
-  constructor(expression, ifBody, elseBody) {
+  constructor(info, expression, ifBody, elseBody) {
+    this.info = info
     this.expression = expression
     this.ifBody = ifBody
     this.elseBody = elseBody
@@ -154,7 +164,8 @@ class IfNode {
   typeCheck(typeEnv) {
     const env = this.expression.type === "boolean" ?
       typeEnv :
-      typeEnv.setIn(["errors", this.line], "Expression must return boolean")
+      typeEnv.setIn(["errors", this.info.get("line")],
+                    "Expression must return boolean")
 
     const exprErrors = this.expression.typeCheck(typeEnv).get("errors")
     const ifErrors = typeCheckEach(this.ifBody, typeEnv).get("errors")
@@ -167,7 +178,8 @@ class IfNode {
 }
 
 class CompareNode {
-  constructor(comparision, left, right) {
+  constructor(info, comparision, left, right) {
+    this.info = info
     this.comparision = comparision
     this.left = left
     this.right = right
@@ -176,7 +188,7 @@ class CompareNode {
   typeCheck(typeEnv) {
     return this.left.type === this.right.type
       ? typeEnv : typeEnv.setIn(
-        ["errors", this.line],
+        ["errors", this.info.get("line")],
         `Comparing mismatching types: ${this.left.type} and ${this.right.type}`)
   }
 }
@@ -198,7 +210,8 @@ const matchingTypes = (t1, t2) =>
       t1 === t2 || isOneString(t1, t2) || isBothNumbers(t1, t2)
 
 class ArithmeticsNode {
-  constructor(operator, left, right) {
+  constructor(info, operator, left, right) {
+    this.info = info
     this.operator = operator
     this.left = left
     this.right = right
@@ -208,7 +221,7 @@ class ArithmeticsNode {
   typeCheck(typeEnv) {
     const env = isValid(this.type) ?
       typeEnv : typeEnv.setIn(
-        ["errors", this.line],
+        ["errors", this.info.get("line")],
         `Can't ${this.operator} ${this.left.type} and ${this.right.type}`)
     return env.update(
       "errors",
@@ -221,7 +234,8 @@ class ArithmeticsNode {
 }
 
 class AssignmentNode {
-  constructor(id, expression, type) {
+  constructor(info, id, expression, type) {
+    this.info = info
     this.id = id
     this.expression = expression
     this.type = type
@@ -244,7 +258,8 @@ class AssignmentNode {
     ].filter(x => x)
 
     const env = errors.length > 0 ?
-      typeEnv.setIn(["errors", this.line], errors.join(",")) : typeEnv
+      typeEnv.setIn(
+        ["errors", this.info.get("line")], errors.join(",")) : typeEnv
 
     const nodeTypeEnv =
       this.type ? env.setIn(["types", this.id], this.type) : env
@@ -305,14 +320,15 @@ function parseValue(type, value) {
 }
 
 class ValueNode {
-  constructor(type, value) {
+  constructor(info, type, value) {
+    this.info = info
     this.type = type
     this.value = parseValue(type, value)
   }
   typeCheck(typeEnv) {
     return isValid(this.value) ?
       typeEnv : typeEnv.setIn(
-        ["errors", this.line],
+        ["errors", this.info.get("line")],
         `Invalid type ${this.type} for value ${this.value}`)
   }
   eval() {
@@ -321,7 +337,8 @@ class ValueNode {
 }
 
 class ArgumentNode{
-  constructor(type, id) {
+  constructor(info, type, id) {
+    this.info = info
     this.type = type
     this.id = id
   }
