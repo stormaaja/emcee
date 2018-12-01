edea462adfc727137c1cc3f05f80d5ca2c91b7fe
Matti Ahinko <matti.ahinko@gmail.com>
2018-12-01 15:28:42 +0200

    Add info map

--- test/eval_arithmetics.test.js
+++ test/eval_arithmetics.test.js
@@ -1,7 +1,11 @@
 const {ValueNode, ArithmeticsNode} = require("../ast.js")
+const { Map } = require("immutable")
 
 test("eval add integers", () => {
   const node = new ArithmeticsNode(
-    "add", new ValueNode("integer", "2"), new ValueNode("integer", "5"))
+    Map({line: 0}),
+    "add",
+    new ValueNode(Map({line: 0}), "integer", "2"),
+    new ValueNode(Map({line: 0}), "integer", "5"))
   expect(node.eval()).toBe(7)
 })
