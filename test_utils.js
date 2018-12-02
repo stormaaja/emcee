27ed137b52c25fbb598961dcfe28dbe00fb96160
Matti Ahinko <matti.ahinko@gmail.com>
2018-12-02 11:28:58 +0200

    Add createInfo

--- test_utils.js
+++ test_utils.js
@@ -11,4 +11,11 @@ function generateProgramNode(children) {
   ])
 }
 
-module.exports = {generateMain, generateProgramNode}
+function createInfo(fl, ll, fc, lc) {
+  return {first_line: fl || 0,
+          last_line: ll || 0,
+          first_column: fc || 0,
+          last_column: lc || 0}
+}
+
+module.exports = {generateMain, generateProgramNode, createInfo}
