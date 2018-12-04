b82665de8b2ebab1e38e2b19d900544cde470ccb
Matti Ahinko <matti.ahinko@gmail.com>
2018-12-04 20:17:09 +0200

    Move running to own file

--- compiler.js
+++ compiler.js
@@ -13,39 +13,3 @@ function compile(files) {
   )
   return parseResult
 }
-
-function main(argv) {
-  if (argv.length < 3) {
-    console.log("No source file given")
-    return 1
-  }
-
-  const args = argv.slice(2)
-  const files = args.filter(f => !f.startsWith("--"))
-  const options = args.filter(f => f.startsWith("--"))
-
-  const objects = options.includes("--objects")
-  const printAST = options.includes("--print-ast")
-  const debug = options.includes("--debug")
-
-  const results = compile(files)
-
-  results.forEach(r => {
-    if (r.success) {
-      console.log("%s: \x1b[32mOK\x1b[0m", r.file)
-      if (printAST) {
-        const tree = objects ? r.result : JSON.stringify(r.result, null, 2)
-        console.log(tree)
-      }
-    } else {
-      console.log("%s: \x1b[31mFail\x1b[0m", r.file)
-      console.log(r.result.message)
-      if (debug) {
-        console.log(r.result)
-      }
-    }
-  })
-  return 0
-}
-
-main(process.argv)
