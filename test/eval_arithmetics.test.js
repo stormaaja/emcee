76d36b8ba002b32e29a6ce1c45505163c6fe9571
Matti Ahinko <matti.ahinko@gmail.com>
2018-12-07 14:26:03 +0200

    Add more advanced arithmetics eval test

--- test/eval_arithmetics.test.js
+++ test/eval_arithmetics.test.js
@@ -3,13 +3,25 @@ const {
 } = require("../ast.js")
 const { Map, List } = require("immutable")
 
-test("eval add integers", () => {
+test("eval arithmetics", () => {
   const node = new ArithmeticsNode(
     Map({}),
-    "add",
-    new ValueNode(Map({}), "integer", "2"),
-    new ValueNode(Map({}), "integer", "5"))
-  expect(node.eval(Map({result: 0})).get("result")).toBe(7)
+    "subtract",
+    new ArithmeticsNode(
+      Map({}),
+      "div",
+      new ValueNode(Map({}), "double", "5.0"),
+      new ArithmeticsNode(
+        Map({}),
+        "add",
+        new ValueNode(Map({}), "integer", "2"),
+        new ValueNode(Map({}), "double", "3.0"))),
+    new ArithmeticsNode(
+      Map({}),
+      "multiply",
+      new ValueNode(Map({}), "double", "3.0"),
+      new ValueNode(Map({}), "integer", "7")))
+  expect(node.eval(Map({result: 0})).get("result")).toBe(-20)
 })
 
 test("eval system function double to string call", () => {
