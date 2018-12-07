e9c6f8bd171017c6eef8cbe67c97d460a0c02b42
Matti Ahinko <matti.ahinko@gmail.com>
2018-12-07 14:14:20 +0200

    Add test for system function call

--- test/eval_arithmetics.test.js
+++ test/eval_arithmetics.test.js
@@ -1,11 +1,21 @@
-const {ValueNode, ArithmeticsNode} = require("../ast.js")
-const { Map } = require("immutable")
+const {
+  ValueNode, ArithmeticsNode, FunctionCallNode
+} = require("../ast.js")
+const { Map, List } = require("immutable")
 
 test("eval add integers", () => {
   const node = new ArithmeticsNode(
-    Map({line: 0}),
+    Map({}),
     "add",
     new ValueNode(Map({}), "integer", "2"),
     new ValueNode(Map({}), "integer", "5"))
   expect(node.eval(Map({result: 0})).get("result")).toBe(7)
+})
+
+test("eval system function call", () => {
+  const node = new FunctionCallNode(
+    Map({}),
+    "d_to_str",
+    List([new ValueNode(Map({}), "double", "2.5")]))
+  expect(node.eval(Map({result: 0})).get("result")).toBe("2.5")
 })
