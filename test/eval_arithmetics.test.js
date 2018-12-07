cd5669cf40a18ec08e652e1cdfb460aa498e0229
Matti Ahinko <matti.ahinko@gmail.com>
2018-12-07 14:01:06 +0200

    Use result

--- test/eval_arithmetics.test.js
+++ test/eval_arithmetics.test.js
@@ -5,7 +5,7 @@ test("eval add integers", () => {
   const node = new ArithmeticsNode(
     Map({line: 0}),
     "add",
-    new ValueNode(Map({line: 0}), "integer", "2"),
-    new ValueNode(Map({line: 0}), "integer", "5"))
-  expect(node.eval()).toBe(7)
+    new ValueNode(Map({}), "integer", "2"),
+    new ValueNode(Map({}), "integer", "5"))
+  expect(node.eval(Map({result: 0})).get("result")).toBe(7)
 })
