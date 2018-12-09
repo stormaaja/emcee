7e4d6e60ceff1b0dffcb1d2dc0cdb6f5040543b5
Matti Ahinko <matti.ahinko@gmail.com>
2018-12-09 08:25:23 +0200

    Change to use update system functions

--- test/eval.test.js
+++ test/eval.test.js
@@ -28,19 +28,21 @@ test("eval arithmetics", () => {
 })
 
 test("eval system function double to string call", () => {
-  const node = new FunctionCallNode(
-    Map({}),
-    "d_to_str",
-    List([new ValueNode(Map({}), "double", "2.5")]))
-  expect(node.eval(Map({result: 0})).get("result")).toBe("2.5")
+  const node = new RootNode(List([
+    new FunctionCallNode(
+      Map({}),
+      "d_to_str",
+      List([new ValueNode(Map({}), "double", "2.5")]))]))
+  expect(node.eval()).toBe("2.5")
 })
 
 test("eval system function integer to string call", () => {
-  const node = new FunctionCallNode(
-    Map({}),
-    "to_str",
-    List([new ValueNode(Map({}), "integer", "2")]))
-  expect(node.eval(Map({result: 0})).get("result")).toBe("2")
+  const node = new RootNode(List([
+    new FunctionCallNode(
+      Map({}),
+      "to_str",
+      List([new ValueNode(Map({}), "integer", "2")]))]))
+  expect(node.eval()).toBe("2")
 })
 
 test("eval if", () => {
